0d518ad5dca2fdd236b4c31b2ab8f265f6993747
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-05-16 20:57:49 -0300

    Criando as regras do jogo

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from "src/assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
@@ -22,7 +23,32 @@ export class QuizzComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
 
+      this.questions = quizz_questions.questions
+      this.questionsSelected = this.questions [this.questionsIndex]
+
+      this.questionsIndex = 0
+      this.questionMax = this.questions.length
+    }
+  }
+
+  playerChoose(value: string){
+    this.answers.push(value)
+    console.log(this.answers)
+    console.log("teste")
+  }
+
+  async nextStep(){
+    this.questionsIndex+=1
+
+    if(this.questionMax > this.questionsIndex){
+      this.questionsSelected = this.questions[this.questionsIndex]
+    }else {
+      this.finished = true
+    }
   }
 
 }
